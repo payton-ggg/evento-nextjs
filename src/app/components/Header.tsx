311c2aacdf4f1df71dc0b6b43377c59f7ac8f7e6
Platon <platon20081019@gmail.com>
2024-12-11 00:46:19 +0200

    header and footer

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>asfds</li>
+        <li>asfdaf</li>
+        <li>adfgag</li>
+        <li>dfgaasd</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/event/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <main>Event page</main>;
+};
+
+export default page;
