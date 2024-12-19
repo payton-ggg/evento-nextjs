58f49b523b3cc96c5208d28da0a55817a69ce8fb
Platon <platon20081019@gmail.com>
2024-12-19 22:28:13 +0200

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Find events around you</h1>
-      <p>Browse more than 10,000 events around you</p>
+    <main className="flex flex-col items-center px-3 pt-36">
+      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
+        Find events around you
+      </h1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
+        Browse more than <span className="italic underline">10,000</span> events
+        around you
+      </p>
 
       <form>
         <input type="text" placeholder="Search events" spellCheck={false} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,29 @@
 import Link from "next/link";
 import Logo from "./Logo";
-
+const links = [
+  {
+    path: "Home",
+    href: "/",
+  },
+  {
+    path: "All events",
+    href: "/events/all",
+  },
+];
 const Header = () => {
   return (
     <header className="flex justify-between border-b border-white/ h-14 items-center px-3 md:px-9">
       <Logo />
       <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/events/all">All events</Link>
-          </li>
+        <ul className="flex gap-x-6 text-sm">
+          {links.map((link) => (
+            <li
+              key={link.path}
+              className="text-white/50 hover:text-white transition"
+            >
+              <Link href={link.href}>{link.path}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
