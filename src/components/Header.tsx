c320d02816aae138a58816bc2e96ca92d056413b
Platon <platon20081019@gmail.com>
2024-12-19 22:17:16 +0200

    asd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,17 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className="flex justify-between">
+    <header className="flex justify-between border-b border-white/ h-14 items-center px-3 md:px-9">
       <Logo />
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/events/all">All events</Link>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/events/all">All events</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
